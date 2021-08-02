f8e75d38e35dbd9660da2246c2f6d4161b5aeb25
Lincoln Araujo <72479081+Lincoln-Araujo@users.noreply.github.com>
2021-08-02 14:44:01 -0300

    Create Checking_Elements_Of_An_Object.js
    This programing can help us to check if inside of some object contains some data we are interested in.

---
+++ Checking_Elements_Of_An_Object.js
@@ -0,0 +1,23 @@
+// This programing can help us to check if inside of some object contains some data we are interested in
+// In the example below we are trying to check if all foods in an array of objects are plant based
+
+// Example
+const isTheDinnerVegan = object => {
+  for (food of object) {
+    if (food.source === 'plant') {
+      return true;
+    } else {
+      return false;
+    }
+  } 
+}
+
+const dinner = [{name: 'hamburger', source: 'meat'}, 
+                {name: 'cheese', source: 'dairy'}, 
+                {name: 'ketchup', source:'plant'}, 
+                {name: 'bun', source: 'plant'}, 
+                {name: 'dessert twinkies', source:'unknown'}
+               ];
+
+console.log(isTheDinnerVegan(dinner))
+// Should print false
