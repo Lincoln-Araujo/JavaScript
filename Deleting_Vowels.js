163402cfb59c37473c824b034d643f7eed7f5c69
Lincoln Araujo <72479081+Lincoln-Araujo@users.noreply.github.com>
2022-05-15 20:48:33 -0300

    Create Deleting_Vowels.js

---
+++ Deleting_Vowels.js
@@ -0,0 +1,22 @@
+// this code will take off all the vowels of a string
+
+function disemvowel(str) {
+  const vowels = ['a','e','i','o','u'];
+  
+  let arrayStr = str.split('');
+  
+  const upper = vowels.map(element => {
+    return element.toUpperCase();
+  });
+  
+  let newArr = []
+  
+  arrayStr.filter(letter => {!vowels.includes(letter) && !upper.includes(letter) ? newArr.push(letter) : letter++ })
+  
+  let intersection = newArr.join('');
+  
+  return intersection;
+}
+
+console.log(disemvowel("hI my name is jon"));
+// the result will be: 'h my nm s jn'
